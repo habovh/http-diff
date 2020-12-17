0f29f0321ef1cc0df6d8f518acac73430700ce88
Jordan Becker <jordanbeckerfr@gmail.com>
2020-12-17 13:43:41 +0100

    feat(config): add brower options

--- index.js
+++ index.js
@@ -4,25 +4,44 @@ import fs from 'fs'
 
 import notifier from './notifier.js'
 
-const configs = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
 
 const main = async () => {
-  const browser = await WebdriverIO.remote({
-    capabilities: { browserName: 'chrome' },
+  const browserConfig = {
+    capabilities: {
+      browserName: config.browser.name || 'chrome',
+    },
     logLevel: 'warn',
-  })
+  }
 
-  for (let index = 0; index < configs.length; index += 1) {
-    const config = configs[index]
+  if (config.browser.name === 'chrome') {
+    browserConfig.capabilities['goog:chromeOptions'] = {
+      binary: config.browser.binary,
+      args: config.browser.args,
+    }
+  }
+  if (config.browser.name === 'firefox') {
+    browserConfig.capabilities['moz:firefoxOptions'] = {
+      args: config.browser.args,
+      binary: config.browser.binary,
+    }
+  }
 
-    if (!config.url) {
+  const browser = await WebdriverIO.remote(browserConfig)
+
+  const watchlist = config.watchlist
+
+  for (let index = 0; index < watchlist.length; index += 1) {
+    const watchEntry = watchlist[index]
+
+    if (!watchEntry.url) {
       console.warn(`Missing required 'url' key in config at index ${index}, skipping.`)
       break
     }
 
-    await browser.navigateTo(config.url)
+    await browser.navigateTo(watchEntry.url)
 
-    const node = await browser.$(config.selector || 'body')
+    const node = await browser.$(watchEntry.selector || 'body')
 
     await node.waitForExist({ timeout: 10000 })
 
@@ -35,17 +54,17 @@ const main = async () => {
     }
 
     // Compare
-    const oldHtml = state[config.url]
+    const oldHtml = state[watchEntry.url]
     if (oldHtml) {
       const diff = diffLines(html, oldHtml, { ignoreWhitespace: true })
       const changes = diff.filter((change) => change.added || change.removed)
       if (changes.length > 0) {
-        await notifier(config, changes)
+        await notifier(watchEntry, changes)
       }
     }
 
     // Store
-    state[config.url] = html
+    state[watchEntry.url] = html
     fs.writeFileSync('./state.json', JSON.stringify(state))
   }
 
